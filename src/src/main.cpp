3ac781d050188affd8a00c2e5e08810cc4445e21
japopich <japopich@gmail.com>
2019-09-10 19:22:33 -0600

    Modified main.cpp to include Libraries

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_MPL3115A2.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
 
 void setup() {
   // put your setup code here, to run once:
+    pinMode(13, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(13, LOW); // RX LED on
+  delay(333);
+  digitalWrite(13, HIGH); // RX LED off
+  delay(333);
 }
