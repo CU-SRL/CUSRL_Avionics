d56a665ce63e3c2f7fb6e40521a662dd50b07de2
japopich <japopich@hotmail.com>
2019-09-12 21:29:06 -0600

    Added BNO055 to the code with a sample running on the threads

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -18,25 +18,77 @@
 #include <Adafruit_BNO055.h>
 #include "Thread.h"
 #include <ThreadController.h>
+#include <utility/imumaths.h>
 
 // ThreadController that will controll all threads
 ThreadController thread_control = ThreadController();
+
+/* Set the delay between fresh samples */
+uint16_t BNO055_SAMPLERATE_DELAY_MS = 10;
+
+// Check I2C device address and correct line below (by default address is 0x29 or 0x28)
+//                                   id, address
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
 
 //Threads (as a pointer)
 Thread* ThreadOne = new Thread();
 Thread* ThreadTwo = new Thread();
 Thread* ThreadBlink = new Thread();
 
-void TestAsyncFuncOne()
+void printEvent(sensors_event_t* event) {
+  Serial.println();
+  Serial.print(event->type);
+  double x = -1000000, y = -1000000 , z = -1000000; //dumb values, easy to spot problem
+  if (event->type == SENSOR_TYPE_ACCELEROMETER) {
+    x = event->acceleration.x;
+    y = event->acceleration.y;
+    z = event->acceleration.z;
+  }
+  else if (event->type == SENSOR_TYPE_ORIENTATION) {
+    x = event->orientation.x;
+    y = event->orientation.y;
+    z = event->orientation.z;
+  }
+  else if (event->type == SENSOR_TYPE_MAGNETIC_FIELD) {
+    x = event->magnetic.x;
+    y = event->magnetic.y;
+    z = event->magnetic.z;
+  }
+  else if ((event->type == SENSOR_TYPE_GYROSCOPE) || (event->type == SENSOR_TYPE_ROTATION_VECTOR)) {
+    x = event->gyro.x;
+    y = event->gyro.y;
+    z = event->gyro.z;
+  }
+
+  Serial.print(": x= ");
+  Serial.print(x);
+  Serial.print(" | y= ");
+  Serial.print(y);
+  Serial.print(" | z= ");
+  Serial.println(z);
+}
+
+void BNO055()
 {
-  	Serial.print("COOL! I'm running TestAsyncFuncOne on: ");
-	Serial.println(millis());
+	//could add VECTOR_ACCELEROMETER, VECTOR_MAGNETOMETER,VECTOR_GRAVITY...
+  sensors_event_t orientationData , angVelocityData , linearAccelData;
+  bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
+  bno.getEvent(&angVelocityData, Adafruit_BNO055::VECTOR_GYROSCOPE);
+  bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
+
+  printEvent(&orientationData);
+  printEvent(&angVelocityData);
+  printEvent(&linearAccelData);
+
+  int8_t boardTemp = bno.getTemp();
+  Serial.print(F("temperature: "));
+  Serial.println(boardTemp);
+
+  delay(10);
 }
 
 void TestAsyncFuncTwo()
 {
-  	Serial.print("And I'm running TestAsyncFuncTwo on: ");
-	Serial.println(millis());
 }
 
 void blink()
@@ -51,15 +103,19 @@ void blink()
 void setup() {
   // put your setup code here, to run once:
 	pinMode(13, OUTPUT);
-    Serial.begin(9800);
+    Serial.begin(115200);
+
+  /* Initialise the sensor */
+  if (!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while (1);
+  }
   // When Configuring threads you have to give it what function to run.
   // Configure ThreadOne
-	ThreadOne->onRun(TestAsyncFuncOne);
-	ThreadOne->setInterval(250);
-
-	// Configure ThreadTwo
-	ThreadTwo->onRun(TestAsyncFuncTwo);
-	ThreadTwo->setInterval(250);
+	ThreadOne->onRun(BNO055);
+	ThreadOne->setInterval(10);
 
   	// Configure ThreadTwo
 	ThreadBlink->onRun(blink);
@@ -67,7 +123,7 @@ void setup() {
 
 	// Adds both threads to the controller
 	thread_control.add(ThreadOne);
-	thread_control.add(ThreadTwo);
+	//thread_control.add(ThreadTwo);
     thread_control.add(ThreadBlink);
 }
 
