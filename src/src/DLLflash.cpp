5edb764252073cc0473dc6943b3c4519e5ba341b
cartermak <cartermak3@gmail.com>
2019-11-07 21:01:55 -0700

    round 2

--- src/src/DLLflash.cpp
+++ src/src/DLLflash.cpp
@@ -1,1 +1,38 @@
 #include "DLLflash.hpp"
+
+DLLtype::DLLtype(void* refData,int dataSize,char* id) {
+    this->refData = refData;
+    this->dataSize = dataSize;
+    strcpy(this->id,id);
+}
+
+int DLLtype::getDataSize() {
+    return dataSize;
+}
+
+char* DLLtype::getID() {
+    return id;
+}
+
+bool DLLtype::writeSample(uint32_t addr_write,SPIFlash* flash) {
+    
+}
+
+DLLflash::DLLflash() {}
+
+void DLLflash::addType(void* refData, int dataSize, char* id) {
+    types.push_back(DLLtype(refData,dataSize,id));
+}
+
+bool DLLflash::writeSample(char* id) {
+
+    // Loop through vector until finding the correct thing
+    for (int i=0;i<types.size;i++) {
+        if (strcmp(id,types[i].getID())) {
+            bool returnVal = types[i].writeSample(addr_next_available,flash);
+            if(returnVal) {addr_next_available += types[i].getDataSize();}
+            return returnVal;
+        }
+    }
+    return false;
+}
--- src/src/DLLflash.hpp
+++ src/src/DLLflash.hpp
@@ -1,18 +1,31 @@
-#include "yonics.hpp"
+#include <Arduino.h>
 #include<vector>
-
-
+#include "StructDefs.hpp"
+#include "SPIMemory.h"
 
 struct Node {
     uint32_t index = -1;
     uint32_t next = -1;
     uint32_t prev = -1;
+    void* data = NULL;
+    int dataSize = 0;
 };
 
 class DLLtype {
-    public:
+    private:
+        Node currNode;
+        Node nextNode;
+        void* refData = NULL;
         uint32_t head = -1;
         uint32_t tail = -1;
+        int dataSize = 0;
+        char id[3];
+    public:
+        DLLtype(void*,int,char*);
+        bool writeSample(uint32_t,SPIFlash*);
+        int getDataSize();
+        char* getID();
+
 
 };
 
@@ -20,7 +33,11 @@ class DLLflash {
     private:
         // Next available address
         uint32_t addr_next_available = 0;
+        std::vector <DLLtype> types;
+        SPIFlash* flash = NULL;
     public:
         DLLflash();
+        void addType(void*,int,char*);
+        bool writeSample(char*);
 
 };
